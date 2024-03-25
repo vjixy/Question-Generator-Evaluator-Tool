3674904bad8f5f6dac7eae385d09819746bd1ee4
JIHAD Al AKL <60856219+vjixy@users.noreply.github.com>
2024-03-25 20:35:17 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Question-Generator-Evaluator-Tool
+
+This repository is designed to process segments of documents, leveraging large language models (LLMs) to autonomously generate questions based on the content. It also provides a feature that allows users to evaluate and rate the quality of the generated questions, ensuring a continuous improvement in the relevance and accuracy of the interactions
+
+<img src="documentation/images/main_screen.png">
+
+## Add Keys to environmental variable
+Add openAi key (OPENAI_API_KEY) in the terminal before running the solution.
+
+## Run The Solution
+To run the solution execute the following command in a terminal
+`streamlit run main.py`
+
---
+++ src/database/sql_lite_service.py
@@ -0,0 +1,161 @@
+from abc import ABC
+from typing import Tuple, List
+import sqlite3
+
+class SqlLiteService(ABC):
+    def __init__(self, database_name = "Question-Generator-Evaluator-Tool"):
+        print("database initialized")
+        self.database_name = database_name + ".db"
+        self.connection = sqlite3.connect(self.database_name, check_same_thread=False)
+        
+        self.initialize_database()
+        
+    def close_connection(self):
+        self.connection.close()
+              
+    def initialize_database(self):
+
+        cursor = self.connection.cursor()
+
+        # Create 'user' table
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS user (
+            user_name TEXT PRIMARY KEY
+        )
+        ''')
+
+        # Create 'document' table
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS document (
+            document_name TEXT,
+            sections_number INTEGER,
+            PRIMARY KEY (document_name, sections_number)
+        )
+        ''')
+
+        # Create 'rating' table
+        # Note: The rating table combines user_name, document_name, and page_number as a composite key to ensure uniqueness.
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS rating (
+            user_name TEXT,
+            document_name TEXT,
+            section_number INTEGER,
+            rating TEXT,
+            generated_question TEXT,
+            ai_model TEXT,
+            FOREIGN KEY (user_name) REFERENCES user(user_name),
+            FOREIGN KEY (document_name) REFERENCES document(document_name),
+            PRIMARY KEY (user_name, document_name, section_number, ai_model)
+        )
+        ''')
+
+        # Commit changes and close the connection
+        self.connection.commit()
+        
+    def check_if_user_exists(self, user_name):
+        cursor = self.connection.cursor()
+        cursor.execute('''
+            SELECT * FROM user
+            WHERE user_name = ?
+        ''', (user_name,))
+        
+        result = cursor.fetchone()
+        if result:
+            return True
+        return False
+        
+    def add_user(self, user_name):
+        if not self.check_if_user_exists(user_name):
+            cursor = self.connection.cursor()
+            cursor.execute('''
+                INSERT INTO user (user_name)
+                VALUES (?)
+            ''', (user_name,))
+            self.connection.commit()
+           
+    def check_if_document_exists(self, document_name):
+        cursor = self.connection.cursor()
+        cursor.execute('''
+            SELECT * FROM document
+            WHERE document_name = ?
+        ''', (document_name,))
+        
+        result = cursor.fetchone()
+        if result:
+            return True
+        return False
+     
+    def add_document(self, document_name, sections_number):
+        if not self.check_if_document_exists(document_name):
+            cursor = self.connection.cursor()
+            cursor.execute('''
+                INSERT INTO document (document_name, sections_number)
+                VALUES (?, ?)
+            ''', (document_name, sections_number))
+            self.connection.commit()
+        
+    def add_rating(self, user_name, document_name, section_number, rating, generated_question, ai_model):
+        cursor = self.connection.cursor()
+        cursor.execute('''
+            INSERT INTO rating (user_name, document_name, section_number, rating, generated_question, ai_model)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (user_name, document_name, section_number, rating, generated_question, ai_model))
+        self.connection.commit()
+        
+    def get_ratings(self, user_name, document_name):
+        cursor = self.connection.cursor()
+        cursor.execute('''
+            SELECT section_number, rating, generated_question, ai_model FROM rating
+            WHERE user_name = ? AND document_name = ?
+        ''', (user_name, document_name))
+        return cursor.fetchall()
+        
+    def check_for_rating(self, user_name, document_name, section_number):
+        # Connect to the SQLite database
+        
+        cursor = self.connection.cursor()
+
+        # Check if there is already a rating for the given user, document, and page
+        cursor.execute('''
+            SELECT rating FROM rating
+            WHERE user_name = ? AND document_name = ? AND section_number = ?
+        ''', (user_name, document_name, section_number))
+        
+        result = cursor.fetchone()
+
+        if result:
+            # A rating exists, display it
+            print(f"User '{user_name}' has already rated document '{document_name}', page {section_number}, with a rating of {result[0]}.")
+       
+    def update_rating(self, user_name, document_name, section_number, rating, generated_question, ai_model):
+        cursor = self.connection.cursor()
+
+        # Check if the rating already exists
+        cursor.execute('''
+            SELECT rating FROM rating
+            WHERE user_name = ? AND document_name = ? AND section_number = ? AND ai_model = ?
+        ''', (user_name, document_name, section_number, ai_model))
+        
+        result = cursor.fetchone()
+
+        if result:
+            # Update the existing rating
+            cursor.execute('''
+                UPDATE rating
+                SET rating = ?
+                WHERE user_name = ? AND document_name = ? AND section_number = ? AND ai_model = ?
+            ''', (rating, user_name, document_name, section_number, ai_model))
+            print(f"Rating updated for user '{user_name}' on document '{document_name}', page {section_number} to {rating}.")
+        else:
+            self.add_rating(user_name, document_name, section_number, rating, generated_question, ai_model)
+            # # Insert a new rating
+            # cursor.execute('''
+            #     INSERT INTO rating (user_name, document_name, section_number, rating)
+            #     VALUES (?, ?, ?, ?)
+            # ''', (user_name, document_name, section_number, rating))
+            print(f"New rating added for user '{user_name}' on document '{document_name}', page {section_number}.")
+
+        # Commit the changes and close the connection
+        self.connection.commit()
+
+            
